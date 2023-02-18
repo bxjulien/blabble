659e5f6630b56f4f5e5d7c551992919f0af28479
Julien <julien.berthoumieux@epitech.eu>
2023-02-18 22:27:39 +0100

    init client with React 95

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,15 @@
 import './App.css';
 
+import { Frame } from 'react95';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Frame
+        style={{ padding: '0.5rem', width: '100%', height: '100%' }}
+      ></Frame>
+    </div>
+  );
 }
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,35 @@
 import './index.css';
 
 import { ApolloClient, InMemoryCache } from '@apollo/client/core';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
 
 import { ApolloProvider } from '@apollo/react-hooks';
 import App from './App';
 import ReactDOM from 'react-dom/client';
 import { YogaLink } from '@graphql-yoga/apollo-link';
+import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
+import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
+import original from 'react95/dist/themes/original';
+import { styleReset } from 'react95';
+
+const GlobalStyles = createGlobalStyle`
+  ${styleReset}
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif}') format('woff2');
+    font-weight: 400;
+    font-style: normal
+  }
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif_bold}') format('woff2');
+    font-weight: bold;
+    font-style: normal
+  }
+  body {
+    font-family: 'ms_sans_serif';
+  }
+`;
 
 const client = new ApolloClient({
   link: new YogaLink({
@@ -15,7 +39,12 @@ const client = new ApolloClient({
 });
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
+  <>
+    <GlobalStyles />
+    <ThemeProvider theme={original}>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </ThemeProvider>
+  </>
 );
